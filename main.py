c564dd9b91e14367a957abc0f0ad36ac109b4538
vishnu <vishnuprakash.khd@gmail.com>
2025-04-08 18:26:45 +0530

    added api

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def read_root():
+    return {"message": "success"}
+
 def main():
-    print("Hello from pythonwithfastapi!")
+    x = 1
+    print("Hello from pythonwithfastapi!", x)
+
+
 
 
 if __name__ == "__main__":
